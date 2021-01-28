7169cbe5f82b8c77d7d8ad40d62dec5dbedfc424
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-01-28 20:00:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,6 +13,6 @@ def assign_rooms(speakers)
   new_array = []
   speakers.each_with_index do |person, index|
   new_array[person] = index
-  "Hello, #{person}! You'll be assigned to room #{index + 1}!"
+  new_array << "Hello, #{person}! You'll be assigned to room #{index + 1}!"
   end
 end
